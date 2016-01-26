f11406c7575f4f242aa67797aada1d34d5770793
James S White <james.white@github.com>
2016-01-26 10:40:22 -0600

    get the key

--- modules/aptly/manifests/init.pp
+++ modules/aptly/manifests/init.pp
@@ -1,7 +1,14 @@
 class aptly(){
   package { 'aptly':
     ensure => latest,
-    require => File['/etc/apt/sources.list.d/aptly.list'] 
+    require => [
+                 File['/etc/apt/sources.list.d/aptly.list'],
+                 Exec['add_aptly_signature']
+               ]
+  }
+
+  exec {'add_aptly_signature':
+    command => "/usr/bin/apt-key adv --keyserver keys.gnupg.net --recv-keys E083A3782A194991"
   }
 
   file { '/etc/apt/sources.list.d/aptly.list':
