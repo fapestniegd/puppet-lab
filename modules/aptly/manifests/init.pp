d041a8a658197546eb558d7e788d813368d0344c
James S White <james.white@github.com>
2016-01-26 10:44:58 -0600

    apt-get update

--- modules/aptly/manifests/init.pp
+++ modules/aptly/manifests/init.pp
@@ -3,12 +3,17 @@ class aptly(){
     ensure => latest,
     require => [
                  File['/etc/apt/sources.list.d/aptly.list'],
-                 Exec['add_aptly_signature']
+                 Exec['add_aptly_signature'],
+                 Exec['aptly_apt_get_update']
                ]
   }
 
   exec {'add_aptly_signature':
     command => "/usr/bin/apt-key adv --keyserver keys.gnupg.net --recv-keys E083A3782A194991"
+  }
+
+  exec {'aptly_apt_get_update':
+    command => "/usr/bin/apt-get update"
   }
 
   file { '/etc/apt/sources.list.d/aptly.list':
